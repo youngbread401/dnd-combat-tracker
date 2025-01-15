6bdce7d4df305597a84ac722b5592557f2970105
Daniel <daniel.a.lakin@gmail.com>
2025-01-14 21:07:23 -0500

    Update gitignore and App.jsx

--- App.jsx
+++ App.jsx
@@ -32,13 +32,13 @@ import { DiceRoller } from './components/DiceModel';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSy8ia6vKnq95_gbO7lnohVbyAQzqBtk4",
-  authDomain: "dndcombattracker-572b0.firebaseapp.com",
-  databaseURL: "https://dndcombattracker-572b0-default-rtdb.firebaseio.com",
-  projectId: "dndcombattracker-572b0",
-  storageBucket: "dndcombattracker-572b0.firebasestorage.app",
-  messagingSenderId: "812186225431",
-  appId: "1:812186225431:web:8da48e238d10db01d14552"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
